bf841644405371e2f0f67a8e6c46c2f9aaf7b174
kuwagata00 <50312468+kuwagata00@users.noreply.github.com>
2021-10-25 16:56:35 +0900

    const

--- src/index.js
+++ src/index.js
@@ -1,10 +1,53 @@
-import "./styles.css";
+/**
+ * const.let
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// let val2 = "let変数";
+// console.log(val2);
+
+// //letは上書きが可能
+// val2 = "valを上書き";
+
+// //letは再宣言は不可
+// let val2 = "letは再宣言不可";
+
+const val3 = "const変数";
+console.log(val3);
+
+//constは上書きも再宣言も不可
+// val3 = "const上書き";
+// console.log("val3");
+
+// const val3 = "const再宣言";
+// console.log(val3);
+
+//constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+// name: "俺",
+// age: 26,
+// };
+// val4.name = "わし";
+// val4.address = "chiba";
+// console.log(val4);
+
+//constで定義した配列はプロパティの変更が可能
+// const val5 = ["cat", "dog"];
+// val5[0] = "bird";
+// val5.push("monky");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+
+const name = "あああ";
+const age = "26";
+//「俺の名前はあああです。年齢は26歳です。」
+
+//従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+console.log(message1);
+
+//テンプレート文字列
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2);
