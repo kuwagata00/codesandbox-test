a252ca6e45b3635362186165eebb165dbf1205cb
kuwagata00 <50312468+kuwagata00@users.noreply.github.com>
2021-10-25 18:01:39 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -51,3 +51,26 @@ console.log(message1);
 //テンプレート文字列
 const message2 = `私の名前は${name}です。年齢は${age}です。`;
 console.log(message2);
+
+/**
+ * アロー関数
+ */
+//従来の関数
+function func1(str) {
+  return str;
+}
+console.log(func1("func1です"));
+
+//アロー関数
+const func2 = (str) => {
+  return str;
+};
+//const func2 = (str) => str; でも可能
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+
+//const func3 = (num1, num2) => num1 + num2; でも可能
+console.log(func3(10, 20));
